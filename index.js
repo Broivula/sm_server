4094c2bf761466b366338c1aacd7a3be895a4f21
Broivula <elias.koivula1@hotmail.com>
2019-12-15 01:34:43 +0200

    Fixed socket.io version --> proof of consept done

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ const https = require('https');
 const fs = require('fs');
 const app = express();
 
-
 const options = {
 	key: fs.readFileSync('./ssl_cert/key.pem'),
-	cert: fs.readFileSync('./ssl_cert/cert.pem')
+	cert: fs.readFileSync('./alt_cert/ca.crt')
 };
+
+const https_server = https.createServer(options, app);
 
 
 
 // --> routing
 
 app.get('/', (req, res) => {
+	console.log('yo, someones here!');
 	res.json({msg: 'yo, bitch'});
 });
 
-const https_server = https.createServer(options, app);
 
 const listener = https_server.listen(3000, () => {
 	console.log('server started, listening on port 3000');
@@ -26,14 +27,29 @@ const listener = https_server.listen(3000, () => {
 
 // --> socket
 
+
+
 const io = require('socket.io')(listener);
 
 io.on('connection', (socket) => {
 	console.log('new connection, lmao');
-	socket.username = "Desktop";
+
 
 	socket.on('new_action', (data) => {
-		const parsed = JSON.parse(data);
-		console.log(socket.username + ' made a new action: ' + parsed);
+		console.log(' made a new action: ' + data);
+		io.sockets.emit('response', {msg: "this is a response"})
 	});
+	
+	socket.on('disconnect', (err) => {
+		console.log('disconnected:');
+		console.log(err);
+	})
+	
+	socket.on('error', (err) => {
+		console.log('error!');
+		console.log(err)
+	});
+
 });
+
+
